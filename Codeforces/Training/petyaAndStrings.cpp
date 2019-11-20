e2d82e1d7100b6ebd6df015f253b3f1064c9dcbe
gabrielpastori <gabrielfigueira@alunos.utfpr.edu.br>
2019-11-19 23:16:12 -0300

    Add Petya and String solution

--- Codeforces/Training/petyaAndStrings.cpp
+++ Codeforces/Training/petyaAndStrings.cpp
@@ -6,15 +6,21 @@ int main(){
     string a,b;
     cin>>a>>b;
     int ca=0,cb=0;
+    bool ok=1;
     for(int i=0;i<a.size();i++){
         ca+=tolower(a[i])-0;
-    }
-    for(int i=0;i<b.size();i++){
         cb+=tolower(b[i])-0;
+        if(ca<cb){
+            cout<<"-1"<<endl;
+            ok=0;
+            break;
+        }
+        if(ca>cb){
+            ok=0;
+            cout<<"1"<<endl;
+            break;
+        }
     }
-    if(ca<cb) cout<<"-1"<<endl;
+    if(ok) cout<<"0"<<endl;
     
-    if(ca==cb) cout<<"0"<<endl;
-
-    if(ca>cb) cout<<"1"<<endl;
 }
