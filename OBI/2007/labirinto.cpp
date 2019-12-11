e73929fbe9d957431ddb04e125fd7b9429ed26ad
gabrielpastori <gabrielfigueira@alunos.utfpr.edu.br>
2019-12-10 23:10:04 -0300

    Add partial Labirinto

---
+++ OBI/2007/labirinto.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+#define MAXN 60
+int n,m,mat[MAXN][MAXN];
+int main(){
+    cin>>n>>m;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=m;j++){
+            cin>>mat[i][j];
+        }
+    }
+
+}
