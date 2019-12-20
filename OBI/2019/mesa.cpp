80bb7a14252a0a1d52810838a0dbd5ed318d2dc5
gabrielpastori <gabrielfigueira@alunos.utfpr.edu.br>
2019-12-19 22:54:57 -0300

    Add Mesa

---
+++ OBI/2019/mesa.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int a,b,posa,posb;
+    cin>>a>>b;
+    
+    posa=a%3;
+    posb=b%3;
+    if(posb==posa){
+        posb++;
+    }
+    posb%=3/
+    if(posa!=1 and posb!=1) cout<<"1"<<endl;
+    if(posa!=0 and posb!=0) cout<<"0"<<endl;
+    if(posa!=2 and posb!=2) cout<<"2"<<endl;
+
+
+
+
+
+}
