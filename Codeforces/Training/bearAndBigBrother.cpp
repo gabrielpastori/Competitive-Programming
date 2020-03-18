af36e2813a53070c515f0e94e008e3cf3f735ca7
gabrielpastori <gabrielfigueira@alunos.utfpr.edu.br>
2020-03-17 23:36:50 -0300

    Addd Bear and Big Brother

---
+++ Codeforces/Training/bearAndBigBrother.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int a,b,t=0;
+    cin>>a>>b;
+    while(a<=b){
+        a*=3;
+        b*=2;
+        t++;
+    }
+    cout<<t<<endl;
+}
