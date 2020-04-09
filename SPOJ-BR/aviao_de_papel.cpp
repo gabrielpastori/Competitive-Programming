2a5868c80a16cfd918fa98b5967348aa00812742
gabrielpastori <gabrielfigueira@alunos.utfpr.edu.br>
2020-04-08 22:21:32 -0300

    Add Aviões de papel

---
+++ SPOJ-BR/aviao_de_papel.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <cstring>
+#include <cmath>
+#include <algorithm>
+using namespace std;
+ 
+int main(){
+	int c,p,f;
+	cin>>c>>p>>f;
+	if((c*f)<=p) cout<<"S"<<endl;
+	else cout<<"N"<<endl;
+ 
+}
