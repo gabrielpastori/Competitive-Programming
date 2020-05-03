9a095dd287add370ad53ef29f6dd3e45d4fd5a5d
gabrielpastori <gabrielfigueira@alunos.utfpr.edu.br>
2020-05-03 20:50:12 -0300

    Add Cometa - COMETA2

---
+++ SPOJ-BR/cometa.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <cstring>
+#include <cmath>
+#include <algorithm>
+using namespace std;
+int main(){
+	int a;
+	cin>>a;
+	int k=(a-1986)/76;
+	cout<<1986+(k+1)*76<<endl;
+}
+ 
