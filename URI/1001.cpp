a110aa0adca1092489cf65702fdca0634874431e
gabrielpastori <gabrielfigueira@alunos.utfpr.edu.br>
2020-06-10 22:35:58 -0300

    Add Problem 1001 - Extremely Basic

---
+++ URI/1001.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a = 0;
+    cin>>a;
+    int b = 0;
+    cin>>b;
+    int x = a+b;
+    cout<<"X = "<<x<<endl;
+    return 0;
+}
