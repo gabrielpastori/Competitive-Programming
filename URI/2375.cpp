ae43645861fde378e8d3fd82a38993354be62c9b
gabrielpastori <gabrielfigueira@alunos.utfpr.edu.br>
2020-06-19 21:57:58 -0300

    Add Problem 2375 - Sedex

---
+++ URI/2375.cpp
@@ -0,0 +1,16 @@
+//Problem: https://www.urionlinejudge.com.br/judge/en/problems/view/2375
+#include <iostream>
+#include <climits>
+#include <stdio.h>
+#include <queue>
+using namespace std;
+
+int main(){
+	int d;
+	cin>>d;
+	int a,l,p;
+	cin>>a>>l>>p;
+	if(d<=a and d<=l and d<=p) cout<<"S"<<endl;
+	else cout<<"N"<<endl;
+
+}
