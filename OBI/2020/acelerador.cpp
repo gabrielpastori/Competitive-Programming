5046428134e5602a63812438b7e22e0d263541da
gabrielpastori <gabrielfigueira@alunos.utfpr.edu.br>
2020-06-26 23:55:19 -0300

    Add Acelerador de partículas

---
+++ OBI/2020/acelerador.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int d;
+    cin>>d;
+    if(d%8==6) cout<<"1"<<endl;
+    if(d%8==7) cout<<"2"<<endl;
+    if(d%8==0) cout<<"3"<<endl;
+}
