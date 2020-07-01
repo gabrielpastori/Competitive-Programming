6a0626f0835ae7683a5f2c5e9c09b53d79b313fa
gabrielpastori <gabrielfigueira@alunos.utfpr.edu.br>
2020-06-30 22:44:14 -0300

    Add Problem 1011 - Sphere

---
+++ URI/1011.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  double R=0;
+  cin>>R;
+  double vol=(4/3.0)*3.14159*R*R*R;
+  cout<<fixed;
+  cout.precision(3);
+  cout<<"VOLUME = "<<vol<<endl;
+  return 0;
+}
