39811175787294608dec99cd1a425b5263b49a45
gabrielpastori <gabrielfigueira@alunos.utfpr.edu.br>
2020-07-02 23:27:30 -0300

    Add Problem 1582 - The Pythagorean Theorem

---
+++ URI/1582.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <climits>
+#include <cmath>
+#include <stdio.h>
+#include <algorithm>
+#include <cstring>
+using namespace std;
+int mdc(int x,int y){
+	if(y==0) return x;
+
+	return mdc(y,x%y);
+}
+int main(){
+	int x,y,z;
+	while (scanf("%d", &x) == 1 and scanf("%d", &y) == 1 and scanf("%d", &z) == 1){
+		
+		if(max(x,max(y,z))== z){
+			swap(x,z);
+		}
+		if(max(x,max(y,z))== y){
+			swap(x,y);
+		}
+		if(x*x==(y*y+z*z)){
+			if(mdc(x,mdc(max(y,z),min(y,z)))==1){
+				cout<<"tripla pitagorica primitiva"<<endl;
+			}else{
+				cout<<"tripla pitagorica"<<endl;
+			}
+
+		}else{
+			cout<<"tripla"<<endl;
+		}
+	}
+}
