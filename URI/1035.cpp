6f9e60c05b5205b3625c233f7401d945c4de7b00
gabrielpastori <gabrielfigueira@alunos.utfpr.edu.br>
2020-07-05 22:38:49 -0300

    Add Problem 1035 - Selection Test 1

---
+++ URI/1035.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+  int a=0,b=0,c=0,d=0;
+  cin>>a>>b>>c>>d;
+  if (b>c && d>a && (c+d)>(a+b) && c>0 && d>0 && a%2==0){
+    cout<<"Valores aceitos"<<endl;
+  }else{
+    cout<<"Valores nao aceitos"<<endl;
+  }
+  return 0;
+}
