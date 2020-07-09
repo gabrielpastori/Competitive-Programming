b7f1540e4a56331ee70acb611a8d8a974ee6ad7c
gabrielpastori <gabrielfigueira@alunos.utfpr.edu.br>
2020-07-08 23:24:08 -0300

    Add Problem 1004 - Simple Product

---
+++ URI/1004.cpp
@@ -0,0 +1,11 @@
+//Problem: https://www.urionlinejudge.com.br/judge/en/problems/view/1021
+#include <iostream>
+using namespace std;
+
+int main(){
+  int a,b=0;
+  cin>>a;
+  cin>>b;
+  int PROD=a*b;
+  cout<<"PROD = "<<PROD<<endl;
+  return 0;
