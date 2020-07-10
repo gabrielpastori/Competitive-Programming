bc2e761c5735b93237e175094eed3fff64a0cc48
gabrielpastori <gabrielfigueira@alunos.utfpr.edu.br>
2020-07-09 21:37:22 -0300

    Add Problem 1016 - Distance

---
+++ URI/1016.cpp
@@ -0,0 +1,11 @@
+//Problem: https://www.urionlinejudge.com.br/judge/en/problems/view/1016
+#include <iostream>
+using namespace std;
+
+int main(){
+  int d=0;
+  cin>>d;
+  cout<<2*d<<" minutos"<<endl;
+
+  return 0;
+}
