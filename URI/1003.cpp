29eb21bd6f07003bde1b40e2f5a6caa00dbfde0d
gabrielpastori <gabrielfigueira@alunos.utfpr.edu.br>
2020-07-20 22:27:18 -0300

    Add Problem 1003 - Simple Sum

---
+++ URI/1003.cpp
@@ -0,0 +1,13 @@
+//Problem: https://www.urionlinejudge.com.br/judge/en/problems/view/1003
+#include <iostream>
+using namespace std;
+
+int main(){
+  int A,B=0;
+  cin>>A;
+  cin>>B;
+  int SOMA=A+B;
+  cout<<"SOMA = "<<SOMA<<endl;
+
+  return 0;
+}
