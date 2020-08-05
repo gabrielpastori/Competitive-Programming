47e09ae573dbc38e33e310daaa6e304c61fea165
gabrielpastori <gabrielfigueira@alunos.utfpr.edu.br>
2020-08-04 22:02:58 -0300

    Fix problem link

--- URI/1152.cpp
+++ URI/1152.cpp
@@ -1,4 +1,4 @@
-//Problem: https://www.urionlinejudge.com.br/judge/pt/problems/view/1088
+//Problem: https://www.urionlinejudge.com.br/judge/pt/problems/view/1152
 #include <iostream>
 #include <vector>
 #include <climits>
